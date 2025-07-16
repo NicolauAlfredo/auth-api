999315a2d8937ac4ff5574df7eedc9f9ef49478c
NicolauAlfredo <b.nicolaualfredo@gmail.com>
2025-07-16 10:07:44 +0200

    Initial project structure and Maven setup for User Auth API

---
+++ src/main/java/com/nicolaualfredo/auth/AuthAPI.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+
+
+/**
+ *
+ * @author nicolaualfredo
+ */
+public class AuthAPI {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
